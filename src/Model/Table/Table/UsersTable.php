fae502103ad5aee43e7edca96cf05019fae315d1
MFORCEHQ1-PC\MFORCEHQ1 <skepong@gmail.com>
2017-05-23 15:52:53 +0800

    add table Users

---
+++ src/Model/Table/Table/UsersTable.php
@@ -0,0 +1,22 @@
+<?php
+    namespace App\Model\Table;
+
+    use Cake\ORM\Query;
+    use Cake\ORM\RulesChecker;
+    use Cake\ORM\Table;
+    use Cake\Validation\Validator;
+
+    class UsersTable extends Table
+    {
+        public function Initialize (array $config)
+        {
+            parent :: Initialize($config);
+
+            $this->setTable('users');
+            $this->setDisplayField('name');
+            $this->setPrimaryKey('id');
+        }
+    }
+
+
+ ?>
